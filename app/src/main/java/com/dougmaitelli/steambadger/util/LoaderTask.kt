0a284cf4d5269ac10f16b9c92959613ebc3c23df
Douglas Maitelli <dougmaitelli@gmail.com>
2019-10-09 22:39:49 -0700

    Progress Dialog Fixes

--- app/src/main/java/com/dougmaitelli/steambadger/util/LoaderTask.kt
+++ app/src/main/java/com/dougmaitelli/steambadger/util/LoaderTask.kt
@@ -2,27 +2,22 @@ package com.dougmaitelli.steambadger.util
 
 import android.app.Activity
 import android.os.AsyncTask
-import android.view.View
-import android.view.ViewGroup
-import android.widget.RelativeLayout
-import android.widget.ProgressBar
+import androidx.appcompat.app.AlertDialog
+import com.dougmaitelli.steambadger.R
 
 abstract class LoaderTask<C : Activity> constructor(context: C, showProgress: Boolean = true) : AsyncTask<C, Any, Any>() {
 
     protected val context: C
-    private var progressBar: ProgressBar? = null
+    private var dialog: AlertDialog? = null
 
     init {
         this.context = context
 
         if (showProgress) {
-            val layout: ViewGroup = context.findViewById(android.R.id.content)
-
-            progressBar = ProgressBar(context, null, android.R.attr.progressBarStyleLarge)
-            val params = RelativeLayout.LayoutParams(100, 100)
-            params.addRule(RelativeLayout.CENTER_IN_PARENT)
-            layout.addView(progressBar, params)
-            progressBar!!.visibility = View.VISIBLE
+            val builder = AlertDialog.Builder(context)
+            builder.setView(R.layout.progress)
+            dialog = builder.create()
+            dialog!!.show()
         }
 
         execute(this.context)
@@ -47,8 +42,8 @@ abstract class LoaderTask<C : Activity> constructor(context: C, showProgress: Bo
 
     override fun onPostExecute(result: Any?) {
         onComplete()
-        if (progressBar != null) {
-            progressBar!!.visibility = View.GONE
+        if (dialog != null) {
+            dialog!!.dismiss()
         }
     }
 
